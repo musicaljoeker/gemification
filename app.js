36f5f8ab746d169fc682615b0b624e9707e1643b
Joseph Kerkhof <musicaljoeker@gmail.com>
2016-11-10 11:11:23 -0600

    changing the port number to 80

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
-var port = process.env.PORT || 443;
+var port = process.env.PORT || 80;
 
 // body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
