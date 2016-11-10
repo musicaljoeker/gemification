c6605a91254944167960f0f2a5968b2cc1afe4ac
Joseph Kerkhof <musicaljoeker@gmail.com>
2016-11-10 16:54:25 -0600

    adding connections to the MySQL database and running a test query

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
+// Requirements
 var express = require('express');
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
 
+// Database Connection
+var db = mysql.createConnection({
+  host      : 'galerafloat.mio.uwosh.edu',
+  user      : 'kerkhofj',
+  password  : 'Oshkosh123!',
+  database  : 'Gemification'
+});
+
+// Server Definitions
 var app = express();
 var port = process.env.PORT || 80;
 
-// body parser middleware
+// Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// test route
+// Test Route
 app.get('/', function (req, res) { res.status(200).send('Hello world!'); });
 
 app.listen(port, function () {
   console.log('Listening on port ' + port);
 });
 
+// Gemification Posts
 app.post('/gem', function (req, res, next) {
   var userFrom = req.body.user_name;
   var text = req.body.text;
@@ -27,4 +39,29 @@ app.post('/gem', function (req, res, next) {
   } else {
     return res.status(200).end();
   }
+});
+
+// This is a test funciton to the MySQL database
+app.post('/show-tables', function (req, res, next) {
+  // Setting up the database connection
+  db.connect(function(err){
+    if (!err) {
+      console.log("Database is connected");
+    } else {
+      // Throw an error
+      console.log("Error in connecting to database: " + err);
+    }
+  });
+
+  // Test query to the database
+  db.query('SHOW TABLES', function(err, rows, fields){
+    db.end();
+    if (!err) {
+      // Do some stuff
+      res.status(200).send("These are the tables:\n" + rows);
+    } else {
+      // Throw an error
+      console.log("Error in performing tables query");
+    }
+  });
 });
