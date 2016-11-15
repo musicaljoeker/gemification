6fc296c305615289f99450891db0095db26defbd
Joseph Kerkhof <musicaljoeker@gmail.com>
2016-11-15 14:43:26 -0600

    update to text on dummy command

--- app.js
+++ app.js
@@ -38,16 +38,13 @@ app.get('/', function (req, res) { res.status(200).send('Hello world!'); });
 server.listen(port, function(){
   console.log('Listening on port ' + port);
 });
-// app.listen(port, function () {
-//   console.log('Listening on port ' + port);
-// });
 
 // Gemification Posts
 app.post('/gem', function (req, res, next) {
   var userFrom = req.body.user_name;
   var text = req.body.text;
   var botResponse = {
-    text : "This is your username " + userFrom + "\nThis is what you entered after your username: " + text
+    text : "This is your username " + userFrom + "\nThis is what you entered after your /gem command: " + text
   };
 
   // Loop otherwise..
