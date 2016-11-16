0bb0820b723239c7ce95edb2d828114a4d205d3f
Joseph Kerkhof <musicaljoeker@gmail.com>
2016-11-16 16:42:39 -0600

    trying with the client secret

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ var pool = mysql.createPool({
 // Gemification Slackbot Configuration
 var controller = botkit.slackbot();
 var bot = controller.spawn({
-  token: "2m0qLeTSddvFfZRScbVZcPFe"
+  token: "a3f0961b70c2ebec94c83b7f507de574"
 })
 bot.startRTM(function(err,bot,payload) {
   if (err) {
