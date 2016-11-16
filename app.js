5f22c65a7fe9494d22420ed8de216b9205518b83
Joseph Kerkhof <musicaljoeker@gmail.com>
2016-11-16 16:28:24 -0600

    removing the API key

--- app.js
+++ app.js
@@ -31,7 +31,9 @@ var pool = mysql.createPool({
 
 // Gemification Slackbot Configuration
 var controller = botkit.slackbot();
-var bot = controller.spawn({});
+var bot = controller.spawn({
+  token: a3f0961b70c2ebec94c83b7f507de574
+})
 bot.startRTM(function(err,bot,payload) {
   if (err) {
     throw new Error('Could not connect to Slack');
