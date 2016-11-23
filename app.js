10d9c332c06f5f71d5921d0307a81e1967c0f232
Joseph Kerkhof <musicaljoeker@gmail.com>
2016-11-23 14:55:10 -0600

    testing a bot on the gemification server

--- app.js
+++ app.js
@@ -1,96 +1,143 @@
-// Requirements
-var fs = require('fs');
-var http = require('http');
-var https = require('https');
-var express = require('express');
-var bodyParser = require('body-parser');
-var mysql = require('mysql');
-var botkit = require('botkit');
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+           ______     ______     ______   __  __     __     ______
+          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
+          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
+           \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
+            \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
 
-// SSL Certificate Configuration
-var httpsOptions = {
-  key   :  fs.readFileSync('ssl/StarMIO.key', 'utf8'),
-  cert  :  fs.readFileSync('ssl/StarMIO-cert.pem', 'utf8'),
-  ca    :  fs.readFileSync('ssl/StarMIO-chain.pem', 'utf8')
+
+This is a sample Slack Button application that adds a bot to one or many slack teams.
+
+# RUN THE APP:
+  Create a Slack app. Make sure to configure the bot user!
+    -> https://api.slack.com/applications/new
+    -> Add the Redirect URI: http://localhost:3000/oauth
+  Run your bot from the command line:
+    clientId=<my client id> clientSecret=<my client secret> port=3000 node slackbutton_bot.js
+# USE THE APP
+  Add the app to your Slack by visiting the login page:
+    -> http://localhost:3000/login
+  After you've added the app, try talking to your bot!
+# EXTEND THE APP:
+  Botkit has many features for building cool and useful bots!
+  Read all about it here:
+    -> http://howdy.ai/botkit
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+/* Uses the slack button feature to offer a real time bot to multiple teams */
+var Botkit = require('../lib/Botkit.js');
+
+if (!process.env.clientId || !process.env.clientSecret || !process.env.port || !process.env.redirectUri) {
+  console.log('Error: Specify clientId clientSecret redirectUri and port in environment');
+  process.exit(1);
 }
 
-// Server Definitions
-var app = express();
-var port = process.env.PORT || 443;
-var server = https.createServer(httpsOptions, app);
 
-// Database Configuration
-var pool = mysql.createPool({
-    connectionLimit : 100, //important
-    host      : 'galerafloat.mio.uwosh.edu',
-    user      : 'gemification',
-    password  : 'car owner drivers seat',
-    database  : 'Gemification',
-    debug     :  false
-});
-
-// Gemification Slackbot Configuration
-var controller = botkit.slackbot();
-var bot = controller.spawn({
-  token: "22500900023.100481484741"
-})
-bot.startRTM(function(err,bot,payload) {
-  if (err) {
-    throw new Error('Could not connect to Slack');
+var controller = Botkit.slackbot({
+  json_file_store: './db_slackbutton_bot/',
+  // rtm_receive_messages: false, // disable rtm_receive_messages if you enable events api
+}).configureSlackApp(
+  {
+    clientId: process.env.clientId,
+    clientSecret: process.env.clientSecret,
+    redirectUri: process.env.redirectUri,
+    scopes: ['bot'],
   }
+);
 
-  // close the RTM for the sake of it in 5 seconds
-  setTimeout(function() {
-      bot.closeRTM();
-  }, 5000);
+controller.setupWebserver(process.env.port,function(err,webserver) {
+  controller.createWebhookEndpoints(controller.webserver);
+
+  controller.createOauthEndpoints(controller.webserver,function(err,req,res) {
+    if (err) {
+      res.status(500).send('ERROR: ' + err);
+    } else {
+      res.send('Success!');
+    }
+  });
 });
 
-// Body Parser Middleware
-app.use(bodyParser.urlencoded({ extended: true }));
 
-// Gemification Posts
-app.post('/gem', function (req, res, next) {
-  var userFrom = req.body.user_name;
-  var text = req.body.text;
-  var botResponse = {
-    text : "This is your username " + userFrom + "\nThis is what you entered after your /gem command: " + text
-  };
+// just a simple way to make sure we don't
+// connect to the RTM twice for the same team
+var _bots = {};
+function trackBot(bot) {
+  _bots[bot.config.token] = bot;
+}
 
-  // Loop otherwise..
-  if (userFrom !== 'slackbot') {
-    return res.status(200).json(botResponse);
+controller.on('create_bot',function(bot,config) {
+
+  if (_bots[bot.config.token]) {
+    // already online! do nothing.
   } else {
-    return res.status(200).end();
-  }
-});
+    bot.startRTM(function(err) {
 
-// This is a test pool connection function to the MySQL database
-function handle_database(req,res) {
-    pool.getConnection(function(err,connection){
-      if (err) {
-        res.json({"code" : 100, "status" : "Error in connection database"});
-        return;
+      if (!err) {
+        trackBot(bot);
       }
-      console.log('connected as id ' + connection.threadId);
-      connection.query("SHOW TABLES",function(err,rows){
-        connection.release();
-        if(!err) {
-          res.json(rows);
+
+      bot.startPrivateConversation({user: config.createdBy},function(err,convo) {
+        if (err) {
+          console.log(err);
+        } else {
+          convo.say('I am a bot that has just joined your team');
+          convo.say('You must now /invite me to a channel so that I can be of use!');
         }
       });
-      connection.on('error', function(err) {
-        res.json({"code" : 100, "status" : "Error in connection database"});
-        return;
-      });
-  });
-}
 
-// Handling the GET request
-app.post('/show-tables', function(req, res){
-  handle_database(req, res);
+    });
+  }
+
 });
 
-// Server Listner
-server.listen(port, function(){
-  console.log('Listening on port ' + port);
+
+// Handle events related to the websocket connection to Slack
+controller.on('rtm_open',function(bot) {
+  console.log('** The RTM api just connected!');
+});
+
+controller.on('rtm_close',function(bot) {
+  console.log('** The RTM api just closed');
+  // you may want to attempt to re-open
+});
+
+controller.hears('hello','direct_message',function(bot,message) {
+  bot.reply(message,'Hello!');
+});
+
+controller.hears('^stop','direct_message',function(bot,message) {
+  bot.reply(message,'Goodbye');
+  bot.rtm.close();
+});
+
+controller.on(['direct_message','mention','direct_mention'],function(bot,message) {
+  bot.api.reactions.add({
+    timestamp: message.ts,
+    channel: message.channel,
+    name: 'robot_face',
+  },function(err) {
+    if (err) { console.log(err) }
+    bot.reply(message,'I heard you loud and clear boss.');
+  });
+});
+
+controller.storage.teams.all(function(err,teams) {
+
+  if (err) {
+    throw new Error(err);
+  }
+
+  // connect all teams with bots up to slack!
+  for (var t  in teams) {
+    if (teams[t].bot) {
+      controller.spawn(teams[t]).startRTM(function(err, bot) {
+        if (err) {
+          console.log('Error connecting bot to Slack:',err);
+        } else {
+          trackBot(bot);
+        }
+      });
+    }
+  }
+
 });
