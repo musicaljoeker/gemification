b3600eca872bc695e684c3fa552fd2ca1592f007
Joseph Kerkhof <musicaljoeker@gmail.com>
2016-12-06 17:09:04 -0600

    removing the sudo and password from the shell script

--- startserver.sh
+++ startserver.sh
@@ -1,2 +1,1 @@
-sudo clientId=22500900023.109209107527 clientSecret=b78d427f6856f0d28eb9f02d0a094544 port=80 redirectUri=http://gemification.mio.uwosh.edu/oauth node app.js > gemification_log.txt 2>&1 &
-Musicaljoe93
+clientId=22500900023.109209107527 clientSecret=b78d427f6856f0d28eb9f02d0a094544 port=80 redirectUri=http://gemification.mio.uwosh.edu/oauth node app.js > gemification_log.txt 2>&1 &
