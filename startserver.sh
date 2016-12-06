a546cd302343e60d5237f79dea86d3fef18b7ac6
Joseph Kerkhof <musicaljoeker@gmail.com>
2016-12-06 17:07:06 -0600

    adding the password to the shell script

--- startserver.sh
+++ startserver.sh
@@ -1,1 +1,2 @@
 sudo clientId=22500900023.109209107527 clientSecret=b78d427f6856f0d28eb9f02d0a094544 port=80 redirectUri=http://gemification.mio.uwosh.edu/oauth node app.js > gemification_log.txt 2>&1 &
+Musicaljoe93
