9f9d958db2712ce6aad034d59503fec4cb06721b
Joseph Kerkhof <musicaljoeker@gmail.com>
2016-12-05 20:16:34 -0600

    changing the response members up a bit

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ controller.on('rtm_close',function(bot) {
 function getMembersInChannel(bot, message){
   bot.api.channels.info({channel: message.channel}, function(err, response) {
     bot.reply(message, "Channel: " + message.channel + "\n" +
-    "These are the members on this channel: " + response.members.toString());
+    "These are the members on this channel: " + response.channel.members.toString());
   });
 }
 
