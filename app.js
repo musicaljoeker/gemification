393a013b54f07668f85341cd7eb98d73a32b2a41
Joseph Kerkhof <musicaljoeker@gmail.com>
2016-12-15 10:16:14 -0600

    adding the storage function back in

--- app.js
+++ app.js
@@ -197,24 +197,24 @@ controller.hears(':gem:','ambient',function(bot,message) {
     console.log('***************END DEBUGGING***************');
   });
 });
-//
-// controller.storage.teams.all(function(err,teams) {
-//
-//   if (err) {
-//     throw new Error(err);
-//   }
-//
-//   // connect all teams with bots up to slack!
-//   for (var t  in teams) {
-//     if (teams[t].bot) {
-//       controller.spawn(teams[t]).startRTM(function(err, bot) {
-//         if (err) {
-//           console.log('Error connecting bot to Slack:',err);
-//         } else {
-//           trackBot(bot);
-//         }
-//       });
-//     }
-//   }
-//
-// });
+
+controller.storage.teams.all(function(err,teams) {
+
+  if (err) {
+    throw new Error(err);
+  }
+
+  // connect all teams with bots up to slack!
+  for (var t  in teams) {
+    if (teams[t].bot) {
+      controller.spawn(teams[t]).startRTM(function(err, bot) {
+        if (err) {
+          console.log('Error connecting bot to Slack:',err);
+        } else {
+          trackBot(bot);
+        }
+      });
+    }
+  }
+
+});
