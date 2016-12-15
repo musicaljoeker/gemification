03180a1360cfa4b0d2e3c44afed82e82e5f6837c
Joseph Kerkhof <musicaljoeker@gmail.com>
2016-12-15 16:31:15 -0600

    removing the @ from the leaderboard usernames

--- app.js
+++ app.js
@@ -284,9 +284,9 @@ controller.hears('leaderboard',['direct_mention','direct_message'],function(bot,
         var leaderboardStr = 'Leaderboard:\n';
         for(var i=0; i<rows.length; i++){
           if(i==rows.length-1){
-            leaderboardStr += (i+1) + ".) <@" + rows[i].username + "> " + rows[i].currentGems;
+            leaderboardStr += (i+1) + ".) <" + rows[i].username + "> " + rows[i].currentGems;
           } else{
-            leaderboardStr += (i+1) + ".) <@" + rows[i].username + "> " + rows[i].currentGems + "\n";
+            leaderboardStr += (i+1) + ".) <" + rows[i].username + "> " + rows[i].currentGems + "\n";
           }
         }
         bot.reply(message, leaderboardStr);
