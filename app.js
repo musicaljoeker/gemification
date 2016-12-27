1a1531cd524c9965912c5d3cb587288860c35027
Joseph Kerkhof <musicaljoeker@gmail.com>
2016-12-27 13:47:54 -0600

    testing the conversation bot

--- app.js
+++ app.js
@@ -402,3 +402,51 @@ controller.hears('clear gems','direct_message',function(bot,message) {
     });
   });
 });
+
+controller.hears('add admin', 'direct_message', function(bot, message){
+  convo.ask({
+        attachments:[
+            {
+                title: 'Do you want to proceed?',
+                callback_id: '123',
+                attachment_type: 'default',
+                actions: [
+                    {
+                        "name":"yes",
+                        "text": "Yes",
+                        "value": "yes",
+                        "type": "button",
+                    },
+                    {
+                        "name":"no",
+                        "text": "No",
+                        "value": "no",
+                        "type": "button",
+                    }
+                ]
+            }
+        ]
+    },[
+        {
+            pattern: "yes",
+            callback: function(reply, convo) {
+                convo.say('FABULOUS!');
+                convo.next();
+                // do something awesome here.
+            }
+        },
+        {
+            pattern: "no",
+            callback: function(reply, convo) {
+                convo.say('Too bad');
+                convo.next();
+            }
+        },
+        {
+            default: true,
+            callback: function(reply, convo) {
+                // do nothing
+            }
+        }
+    ]);
+});
