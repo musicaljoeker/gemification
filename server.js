05afd91a1f678bcfd277feae3d0e06aaa2a2cc14
Joseph Kerkhof <musicaljoeker@gmail.com>
2017-01-12 16:10:40 -0600

    changing the listening port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ https.createServer({
   key: fs.readFileSync('./ssl/StarMIO.key', 'utf8'),
   cert: fs.readFileSync('./ssl/StarMIO-cert.pem', 'utf8'),
   ca: fs.readFileSync('./ssl/StarMIO-chain.pem', 'utf8')
-}, app).listen(55555);
+}, app).listen(443);
 
 app.get('/', function(req, res){
   res.header('Content-type', 'text/html');
