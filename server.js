df8a2694c6b982613205482d4c0158a0e295c471
Joseph Kerkhof <musicaljoeker@gmail.com>
2017-01-12 15:56:55 -0600

    second try at https

--- server.js
+++ server.js
@@ -1,26 +1,13 @@
-var express = require('express');
-var http = require('http');
-var https = require('https');
-var fs = require('fs');
-var app = express();
-var httpPort = 80;
-var httpsPort = 443;
-var httpsCredentials = {
+var fs = require('fs'),
+https = require('https'),
+express = require('express'),
+app = express();
+
+https.createServer({
   key: fs.readFileSync('./ssl/StarMIO.key', 'utf8'),
   cert: fs.readFileSync('./ssl/StarMIO-cert.pem', 'utf8'),
   ca: fs.readFileSync('./ssl/StarMIO-chain.pem', 'utf8')
-};
-
-// var httpServer = http.createServer(app);
-var httpsServer = https.createServer(httpsCredentials, app);
-
-app.listen(httpPort, function(){
-  console.log('HTTP server running on port ' +  httpPort);
-});
-
-app.listen(httpsPort, function(){
-  console.log('HTTPS server running on port ' +  httpsPort);
-});
+}, app).listen(55555);
 
 app.get('/', function(req, res){
   res.header('Content-type', 'text/html');
